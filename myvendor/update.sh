064de8e93e1bb29c3c01f41ed8ba813ab5374971
无在无不在 <2227627947@qq.com>
2021-11-13 20:29:45 +0800

    fix git bug

--- myvendor/update.sh
+++ myvendor/update.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 path="$HOME/src/siusiu"
-cd $path && git reset --hard && git pull origin master --allow-unrelated-histories && go build ./... && go build && echo "更新已完成，请exit退出，重新进入siusiu"
+cd $path && git reset --hard && git pull origin master 
+go build ./... && go build && echo "更新已完成，请exit退出，重新进入siusiu"
