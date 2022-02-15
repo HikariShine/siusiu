e6e643847010c92b38169ce2b303071a36ea2ac2
无在无不在 <2227627947@qq.com>
2022-02-15 13:28:32 +0800

    add smtp-user-enum

--- routers/routers.go
+++ routers/routers.go
@@ -252,6 +252,19 @@ func Init(shell *ishell.Shell) error {
 			exec.CmdExec("docker", params...)
 		},
 	})
+	shell.AddCmd(&ishell.Cmd{
+		Name: "smtp-user-enum",
+		Help: "SMTP用户名枚举工具",
+		Func: func(c *ishell.Context) {
+			currentDir, err := os.Getwd()
+			if err != nil {
+				log.Println("os.Getwd failed,err:", err)
+				return
+			}
+			params := append([]string{"run", "--rm", "-it", "--network", "host", "-v", currentDir + ":/tmp", "-w", "/tmp", "rickshang/smtp-user-enum"}, c.Args...)
+			exec.CmdExec("docker", params...)
+		},
+	})
 	//未找到命令时
 	shell.NotFound(controllers.NotFoundHandler)
 	return nil
